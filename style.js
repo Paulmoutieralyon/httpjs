b32d30bc7cd0fe230f8a69af0a5a99ec25cccf4f
Paul Moutier <42945383+Paulmoutieralyon@users.noreply.github.com>
2018-09-17 17:08:40 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,10 +1,10 @@
-// Récupère les éléments du DOM
+// Récupère 
 const btn = document.getElementById("myBtn");
 const input = document.getElementById("myInput");
 const meteo = document.getElementById('meteo');
 const myForm = document.getElementById("myForm");
 
-// Permet d'utiliser "Entrée" sans envoyer le formulaire
+//  "Entrée" 
 const formSubmit = function(event) {
     event.preventDefault();
     getTown();
@@ -14,21 +14,21 @@ const getTown = function(){
     getWeather(input.value);
 };
 
-// Gère les evènements (clic sur le bouton et "Entrée" dans l'input)
+
 myForm.addEventListener("submit", formSubmit, true);
 btn.addEventListener("click", getTown, true);
 
-// Récupère les données et les affiche
+
 function getWeather(town){
-    // Interroge l'api openWeather avec la ville entrée en paramètre
+    
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town}&appid=b9ec2fae71f20bc96dc9154a4ab5db5e`)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            // Arrondi la température à un nombre après la virgule
+            
             let temp = (data.main.temp - 273.15).toFixed(1) + "°C";
-            // Affiche la météo | description + température
+          
             meteo.innerHTML = data.weather[0].description + "<br>" + temp;
         }
     );
